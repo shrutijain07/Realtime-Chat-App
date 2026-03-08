c4034272bbb6537b0f00e3673178d42d46dbf045
Shruti Jain <jainshruti1479@gmail.com>
2026-03-08 20:35:32 +0530

    refactor: implement leave room button, improve code formatting and consistency in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,60 @@
-const roomName = document.getElementById('room-name');
-const chatForm = document.getElementById('chat-form')
-const chatMessages = document.querySelector('.chat-messages')
-const usersLi = document.getElementById('users')
+const roomName = document.getElementById("room-name");
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const usersLi = document.getElementById("users");
 
 // Get username and room name from query params
-const {username, room} = Qs.parse(location.search,{
-  ignoreQueryPrefix : true
-})
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
-const socket = io()
+const socket = io();
 
 // Join room
-socket.emit('joinRoom',{username, room})
+socket.emit("joinRoom", { username, room });
 
 // Get User lists
-socket.on('roomUsers',({room,users})=>{
-  outputRoomName(room)
-  outputUsers(users)
-})
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
 
 // Message from server
-socket.on('message',message=>{
-  outputMessage(message)
-  chatMessages.scrollTop = chatMessages.scrollHeight
-})
-chatForm.addEventListener('submit',(e)=>{
-  e.preventDefault()
-  const message = e.target.elements.msg.value
+socket.on("message", (message) => {
+  outputMessage(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = e.target.elements.msg.value;
   // Emit message to server
-  socket.emit('chatMessage',message)
-  e.target.elements.msg.value = ''
-  e.target.elements.msg.focus()
-})
-function outputMessage(msg){
-  const div = document.createElement('div')
-  div.classList.add('message')
+  socket.emit("chatMessage", message);
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
+
+function outputMessage(msg) {
+  const div = document.createElement("div");
+  div.classList.add("message");
   div.innerHTML = `<p class="meta">${msg.user} <span>${msg.time}</span></p>
-  <p class="text">${msg.msg}</p>`
-  document.querySelector('.chat-messages').appendChild(div)
+  <p class="text">${msg.msg}</p>`;
+  document.querySelector(".chat-messages").appendChild(div);
 }
-function outputRoomName(room){
-  roomName.innerText = room
+
+function outputRoomName(room) {
+  roomName.innerText = room;
 }
-function outputUsers(users){
-  usersLi.innerHTML= `${users.map(user=>`<li>${user.username}</li>`).join('')}`
+
+function outputUsers(users) {
+  usersLi.innerHTML = `${users.map((user) => `<li>${user.username}</li>`).join("")}`;
 }
+
+// leaving chat room
+document.getElementById("leave-btn").addEventListener("click", () => {
+  const leaveRoom = confirm("Are you sure you want to leave the chatroom?");
+
+  if (leaveRoom) {
+    window.location = "../index.html";
+  }
+});
